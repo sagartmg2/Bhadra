978e7c1da15fee1d5ec089af7c102284983a144e
Sagar <dev.sagartmg@gmail.com>
2022-09-25 18:14:52 +0545

    html form

---
+++ js/index.js
@@ -0,0 +1,46 @@
+
+function handleSubmit()
+{
+    console.log("form submitteed");
+}
+
+ console.log(document.getElementById("t-body"));
+ document.getElementById("t-body").innerHTML = ""
+
+console.log("js file");
+// alert("linked")
+// let todos = [];
+
+fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(response => response.json())
+    .then(json => {
+
+        // [
+        //     {
+        //         id,title,status
+        //     }
+        // ]
+
+        // DOM
+
+        // data in array 
+
+        // get the t-body
+
+        // template literal
+
+        // document.getElementById
+
+        // <tr>
+        //         <td>laptop</td>
+        //         <td>2</td>
+        //         <td>10</td>
+        //         <td>10</td>
+        //     </tr>
+
+
+        // inser tinside t-body
+
+
+    })
+

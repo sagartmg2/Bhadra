ba5dae44c686354ea3b0014003d8abeb3c3ae28b
Sagar <dev.sagartmg@gmail.com>
2022-10-09 18:12:47 +0545

    html form

--- js/index.js
+++ js/index.js
@@ -1,11 +1,28 @@
 
-function handleSubmit()
-{
-    console.log("form submitteed");
-}
 
- console.log(document.getElementById("t-body"));
- document.getElementById("t-body").innerHTML = ""
+let form = document.getElementById("form")
+form.addEventListener("submit", (event) => {
+    console.log("submitted....");
+    event.preventDefault();
+    // let slug = document.querySelector("#slug")
+    // console.log(slug.value);
+    console.log(event.target.phone.value)
+    console.log(event.target.code.value)
+    console.log(event.target.password.value)
+
+    // fetch api 
+    // fetch('https://jsonplaceholder.typicode.com/todos/' + slug.value)
+    //     .then(response => response.json())
+    //     .then(json => console.log(json))
+})
+
+// function handleSubmit()
+// {
+//     console.log("form submitteed");
+// }
+
+console.log(document.getElementById("t-body"));
+document.getElementById("t-body").innerHTML = ""
 
 console.log("js file");
 // alert("linked")
